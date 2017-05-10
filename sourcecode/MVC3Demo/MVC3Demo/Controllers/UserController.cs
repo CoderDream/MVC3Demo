998ccd13245ac089bfeed8addc1b2c66dd9d878b
CoderDream <coderdream@gmail.com>
2017-05-10 15:09:59 +0800

    add version 0.02

---
+++ sourcecode/MVC3Demo/MVC3Demo/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC3Demo.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //登陆控制器
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]//登陆控制器
+        public ActionResult Login(Models.LoginModel loginModel)
+        {
+            if (loginModel.UserName == "张三" && loginModel.Password == "123456")
+                Response.Write("正确！");
+            else
+                Response.Write("不正确！");
+            return View();
+        }
+
+    }
+}
